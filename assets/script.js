9f5b3365debe46ccb3809138d2ee36b3592f6f76
横澤大輝 <yokozawadaiki@zawaMac.local>
2022-04-02 18:18:43 +0900

    フェードインの実装

--- assets/script.js
+++ assets/script.js
@@ -141,3 +141,50 @@ $(window).on('load',function(){
 
 //})   ！！右側カートサイドバーを使うときの記載！！
 
+
+//フェードインの動き//
+
+function fadeAnime(){
+
+	//ふわっと動くきっかけのクラス名と動きのクラス名の設定
+	$('.fadeInUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+	　　var elemPos = $(this).offset().top-50; //要素より、50px上の
+	　　var scroll = $(window).scrollTop();
+	　　var windowHeight = $(window).height();
+	　　if (scroll >= elemPos - windowHeight){
+	　　$(this).addClass('fadeIn');
+	　　// 画面内に入ったらfadeInというクラス名を追記
+	　　}else{
+	　　　$(this).removeClass('fadeIn');
+	　　// 画面外に出たらfadeInというクラス名を外す
+	　　}
+	　　});
+	
+	//関数でまとめることでこの後に違う動きを追加することが出来ます
+	$('.fadeInDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
+	　　var elemPos = $(this).offset().top-50; //要素より、50px上の
+	　　var scroll = $(window).scrollTop();
+	　　var windowHeight = $(window).height();
+	　　if (scroll >= elemPos - windowHeight){
+	　　$(this).addClass('fadeDown');
+	　　// 画面内に入ったらfadeDownというクラス名を追記
+	　　}else{
+	　　　$(this).removeClass('fadeDown');
+	　　// 画面外に出たらfadeDownというクラス名を外す
+	　　}
+	　　});
+	
+	
+	}//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+	
+	// 画面をスクロールをしたら動かしたい場合の記述
+		$(window).scroll(function (){
+			fadeAnime();/* アニメーション用の関数を呼ぶ*/
+		});// ここまで画面をスクロールをしたら動かしたい場合の記述
+	
+	// 画面が読み込まれたらすぐに動かしたい場合の記述
+		$(window).on('load', function(){
+			fadeAnime();/* アニメーション用の関数を呼ぶ*/
+		});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+		//フェードインここまで//
