6a973f2fdb6e9890a1c16651d19135047ffcd2c8
横澤大輝 <yokozawadaiki@zawaMac.local>
2022-04-03 21:05:47 +0900

    スライダーのカスタマイズ

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 
-//	$('.slider-wrap').slick({
-//		fade:true,//切り替えをフェードで行う。初期値はfalse。
-//		autoplay: true,//自動的に動き出すか。初期値はfalse。
-//		autoplaySpeed: 6000,//次のスライドに切り替わる待ち時間
-//		speed:1500,//スライドの動きのスピード。初期値は300。
-//		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-//		slidesToShow: 1,//スライドを画面に3枚見せる
-//		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-//		arrows: true,//左右の矢印あり
-//		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-//		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-//		dots: true,//下部ドットナビゲーションの表示
-//        pauseOnFocus: false,//フォーカスで一時停止を無効
-//        pauseOnHover: false,//マウスホバーで一時停止を無効
-//        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-//});
+	$('.slider-wrap').slick({
+		fade:true,//切り替えをフェードで行う。初期値はfalse。
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 6000,//次のスライドに切り替わる待ち時間
+		speed:1500,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows: true,//左右の矢印あり
+		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		dots: true,//下部ドットナビゲーションの表示
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
 
 //スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
 $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
@@ -22,28 +22,7 @@ $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
 });
 
 
-//var $slide = $(".slider-wrap")
-//  .slick({
-//    infinite: true,
-//    slidesToShow: 1,
-//    slidesToScroll: 1,
-//    arrows: false,
-//    fade: true,
-//    speed: 2000,
-//    autoplaySpeed: 4000,
-//    autoplay: true
-//  })
-//  .on({
-//    beforeChange: function(event, slick, currentSlide, nextSlide) {
-//      $(".slick-slide", this).eq(currentSlide).addClass("preve-slide");
-//      $(".slick-slide", this).eq(nextSlide).addClass("slide-animation");
-//    },
-//    afterChange: function() {
-//      $(".preve-slide", this).removeClass("preve-slide　slide-animation");
-//    }
-//  });
-//$slide.find(".slick-slide").eq(0).addClass("slide-animation");
-//
+
 
 //任意のタブにURLからリンクするための設定
 function GethashID (hashIDName){
